7dd26450958d69b3f94e2c1661ad46a6314c7e02
John Simon <john@johnsoft.com>
2015-01-07 14:00:50 -0500

    Fix VM symlink issues in packrat develop script

--- packrat/develop.sh
+++ packrat/develop.sh
@@ -14,10 +14,10 @@ run() {
 
 export NODE_ENV=development
 
-run ../node_modules/.bin/tsc -m commonjs --noImplicitAny -w daemon.ts
-run ../node_modules/.bin/supervisor -w ../lib -n exit daemon.js
+run ../node_modules/typescript/bin/tsc -m commonjs --noImplicitAny -w daemon.ts
+run ../node_modules/supervisor/lib/cli-wrapper.js -w ../lib -n exit --debug daemon.js
 
 # to debug, add '--debug' to the node command and uncomment this line
-#run ../node_modules/.bin/node-inspector
+run ../node_modules/node-inspector/bin/inspector.js
 
 wait
